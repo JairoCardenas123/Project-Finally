fb1f2037ffb99c4297ef7e98b6656b8870485f86
JairoCardenas123 <CardenasJairo102011@gmail.com>
2023-10-11 14:03:29 -0500

    feat: :bug: Arreglando Create, Api

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv')
+const conectarDB = require('./config/config.js')
+const express = require('express')
+const app = express()
+const Server = require('./Models/Server.js')
+const cors = require('cors')
+const corsOptions ={
+    origin:'*',
+    credentials:true,
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions))
+
+dotenv.config();
+const server = new Server();
+
+conectarDB();
+
+
+server.listen()
